d91f8c67dfd0e9bfb983d8808605bec4d25255b0
Justin Stoddard <justin.stoddard@go1.com>
2021-06-05 14:36:32 -0700

    added api calls

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,36 @@
-const weatherAPiKey = process.env.WEATHER_API_KEY;
-const apiURL = ``;
+const weatherAPiKey = "f46a6b432cc759cfa77ed4d2d06d5ecf";
 
-fetch();
+const searchForLocation = async (cityName) => {
+  let forecastData;
+  let uvIndex;
+  const forecastApiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherAPiKey}`;
+
+  await fetch(forecastApiURL).then(res => {
+    return res.json();
+  }).then(data => {
+    console.log(data);
+    forecastData = data;
+  }).catch(err => {
+    console.log(`There was an error fetching data [${err.message}]`);
+    throw new Error(err.message);
+  });
+
+  const { lat, lon } = forecastData.city.coord;
+  const oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherAPiKey}`;
+
+  await fetch(oneCallApiUrl).then(res => {
+    return res.json();
+  }).then(data => {
+    console.log(data.current.uvi);
+    uvIndex = data.current.uvi;
+  }).catch(err => {
+    console.log(`There was an error fetching data [${err.message}]`);
+    throw new Error(err.message);
+  });
+};
+
+const searchButton = document.getElementById("forecast-search-button");
+searchButton.addEventListener('click', () => {
+  const searchInputValue = document.getElementById("search-input").value || "";
+  searchForLocation(searchInputValue);
+});
